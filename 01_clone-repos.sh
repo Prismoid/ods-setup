8e8979abff12bcde203f06c88b23c707877ba792
Prismoid <prismoid@example.jp>
2026-06-20 23:25:03 +0900

    fix 01_clone-repos.sh by adding fluentd-async: true

--- 01_clone-repos.sh
+++ 01_clone-repos.sh
@@ -9,3 +9,10 @@ git clone --branch=v1.0.0 --depth=1 https://github.com/open-dataspaces/L3-identi
 git clone --branch=v1.0.0 --depth=1 https://github.com/open-dataspaces/DCS-Payment.git
 
 sed -i.bak 's/postgres:18-alpine/postgres:16-alpine/g' ./payment/docker-compose.yml
+sed -i.bak 's/localhost:24224/127.0.0.1:24224/g' ./l2/docker-compose.yml
+
+if ! grep -q 'fluentd-async:' ./l2/docker-compose.yml; then
+  sed -i.bak '/fluentd-address:/a\
+        fluentd-async: "true"
+' ./l2/docker-compose.yml
+fi
