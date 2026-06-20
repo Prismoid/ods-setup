3f9630ca2744efc8d2a705062e5d871efbda0b76
Prismoid <prismoid@example.jp>
2026-06-20 17:17:48 +0900

    fix sed command

--- 01_clone-repos.sh
+++ 01_clone-repos.sh
@@ -8,4 +8,4 @@ git clone --branch=v1.0.0 --depth=1 https://github.com/open-dataspaces/L2-dp-web
 git clone --branch=v1.0.0 --depth=1 https://github.com/open-dataspaces/L3-identity-component.git
 git clone --branch=v1.0.0 --depth=1 https://github.com/open-dataspaces/DCS-Payment.git
 
-sed -i 's/postgres:18-alpine/postgres:16-alpine/' ./payment/docker-compose.yml
+sed -i.bak 's/postgres:18-alpine/postgres:16-alpine/g' ./payment/docker-compose.yml
